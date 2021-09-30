fa154b2259da56fdd4b6bef566240fd852d64dc9
424679 <91246978+424679@users.noreply.github.com>
2021-09-30 16:32:19 +0800

    對角線亮燈

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
-for (let Y = 0; Y <= 4; Y++) {
-    for (let X = 0; X <= 4; X++) {
-        led.plot(X, Y)
-        basic.pause(100)
-    }
-}
 basic.forever(function () {
-	
+    for (let X = 0; X <= 4; X++) {
+        for (let Y = 0; Y <= 4; Y++) {
+            led.plot(X, X)
+            basic.pause(100)
+            led.unplot(X, X)
+            basic.pause(100)
+        }
+    }
+    basic.clearScreen()
 })
