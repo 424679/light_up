8554712779a493f4d4101ed5b27f808b4c672cac
424679 <91246978+424679@users.noreply.github.com>
2021-10-28 16:46:56 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,25 @@
-basic.forever(function () {
-    for (let X = 0; X <= 4; X++) {
-        for (let Y = 0; Y <= 4; Y++) {
-            led.plot(X, X)
-            basic.pause(100)
-            led.unplot(X, X)
-            basic.pause(100)
-        }
+function 燈數 (num: number) {
+    if (num < 6) {
+        X = num - 1
+        Y = 0
+    } else if (num >= 6 && num < 9) {
+        X = 4
+        Y = num - 5
+    } else if (num >= 9 && num < 14) {
+        X = 13 - num
+        Y = 4
+    } else if (num >= 14 && num <= 16) {
+        X = 0
+        Y = 17 - num
+    } else {
+        led.plot(X, Y)
     }
-    basic.clearScreen()
+}
+let Y = 0
+let X = 0
+let a = 0
+basic.forever(function () {
+    a += 1
+    燈數(a)
+    basic.pause(100)
 })
